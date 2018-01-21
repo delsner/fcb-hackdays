42c22afc8ae8b0822e8270748addf727d0dbc61e
Markus Springer <markus.springer1994@googlemail.com>
2018-01-21 11:10:51 +0100

    Countdown refactoring

--- frontend/src/app/quiz/quiz.component.ts
+++ frontend/src/app/quiz/quiz.component.ts
@@ -18,19 +18,18 @@ export class QuizComponent implements OnInit {
     @Output() submitted: EventEmitter<boolean> = new EventEmitter();
 
 
-
     public userAnswers = [];
 
     public actQuestionIndex = 0;
 
     public actQuestion: Question;
 
+    private countdownTimeout;
+
     constructor(private quizService: QuizService,
                 private router: Router,
                 private rd: Renderer2) {
         //TODO: reroute if not ok here
-
-
     }
 
     ngOnInit() {
@@ -40,9 +39,18 @@ export class QuizComponent implements OnInit {
     }
 
     private startTimer() {
-        setInterval(() => {
-            this.value = this.value - 1.7;
+
+        this.countdownTimeout = setInterval(() => {
+            if (this.value > 0) {
+                console.log("runterzählen");
+                this.value = this.value - (100 / 60);
+            } else {
+                console.log("submit");
+                this.submitQuiz();
+            }
         }, 1000);
+
+
     }
 
 
@@ -61,6 +69,8 @@ export class QuizComponent implements OnInit {
     }
 
     public submitQuiz() {
+        clearInterval(this.countdownTimeout);
+
         this.quizService.verifyQuiz(this.data._id.$oid, this.userAnswers).subscribe((res) => {
             this.router.navigateByUrl('/highscore');
         });
